53404ceaf567396faa7b1c4d1d04a57ebcd4a3f9
dunland <david.unland@web.de>
2022-07-05 12:57:12 +0200

    rename iteration_images

--- public/js/dataView.js
+++ public/js/dataView.js
@@ -38,14 +38,15 @@ function updateSimulationOutputs() {
 function renewDataViewGAMAImgSrcPath(iteration_number, json) {
     const arg = "iteration_round_" + iteration_number
     GAMASimulationImgSrcPaths[arg] = json
+    console.log("updating images src path");
 }
 
 function renewDataViewGAMAImgsPerSection(section_number) {
     const sectionClassName = ".graphs_wrapper_" + section_number + " > div"
     let image_index_counter = 0
     $(sectionClassName).children().each(function () {
-        $(this).attr("src", GAMASimulationImgSrcPaths["iteration_round_" + section_number]["images"][image_index_counter])
-        image_index_counter ++
+        $(this).attr("src", GAMASimulationImgSrcPaths["iteration_round_" + section_number]["iteration_images"][image_index_counter]);
+        image_index_counter++;
     });
 }
 
