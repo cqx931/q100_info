883f4daa8315aa2cd104496ff6cce7e02bbb78e0
dunland <unland@uni-bremen.de>
2022-09-02 15:21:27 +0200

    properly update energy prices scenario data

--- public/js/dataViewTotal.js
+++ public/js/dataViewTotal.js
@@ -2,9 +2,6 @@
 
 
 const renewResultsImages = function(data){
-    console.log(data)
-
-    document.getElementById("energy_prices").src = data.energy_prices + "?update=" + new Date();
+    document.getElementById("energy_prices").src = data.energy_prices + "?update=" + new Date().getTime();
     document.getElementById("emissions_neighborhood_accu").src = data.emissions_neighborhood_accu + "?update=" + new Date();
 }
-
